0b65d683b234671983d0795a1656f733c920edf8
ppmo po <you@example.com>
2024-09-27 22:05:40 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomeComponent, NavbarComponent]
+})
+export class AppComponent {
+  title = 'ng18app-demo';
+}
